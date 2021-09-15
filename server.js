0033c93f44a338b5902596181ee421b7fe3eb042
todaycode <onetoday1030@gmail.com>
2021-09-15 21:36:14 +0900

    updated

--- server.js
+++ server.js
@@ -23,11 +23,13 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
+app.use((req, res) => {
+  res.send('This is Chad API server!');
+});
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
